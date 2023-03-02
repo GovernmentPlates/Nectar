2313a05e77f7efec92f91fa73c1731583537a3dd
Dominic H <dom@dominic.sk>
2023-03-02 14:59:29 +0100

    Initial upload

---
+++ main.py
@@ -0,0 +1,69 @@
+"""
+Nationwide Exchange Rate Calculator
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This script uses Selenium to scrape the exchange rates from the Nationwide website.
+Rates shown are for GBP (£) to the currency specified.
+
+Basic Usage:
+    >>> from main import ExchangeRateCalculator
+    >>> ExchangeRateCalculator().get_last_updated()
+    '02/03/2023'
+    >>> ExchangeRateCalculator().get_exchange_rate('CHF')
+    1.1012
+"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+
+
+class ExchangeRateCalculator:
+    def __init__(self):
+        self.chrome_options = Options()
+        self.chrome_options.add_argument('--headless')
+        self.driver = webdriver.Chrome(options=self.chrome_options)
+        self.base_url = 'https://www.nationwide.co.uk/help/payments/foreign-exchange-rates/'
+
+    def get_last_updated(self) -> str:
+        r"""
+        Returns the last updated date of the exchange rates.
+
+        :return str last_updated: The last updated date of the exchange rates in the format 'dd/mm/yyyy'
+        """
+        self.driver.get(self.base_url)
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(ec.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div/main/div/div['
+                                                             '2]/div/div/div/div/div/div[2]/div/p[1]')))
+        last_updated = self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/main/div/div['
+                                                          '2]/div/div/div/div/div/div[2]/div/p[1]').text
+        return last_updated[16:]
+
+    def _get_exchange_rates(self) -> dict:
+        r"""
+        Returns a dictionary of the exchange rates.
+
+        :return dict exchange_rates: A dictionary of the exchange rates in the format {'currency': 'exchange_rate'}
+        """
+        self.driver.get(self.base_url)
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(ec.presence_of_element_located((By.XPATH, '//*[@id="main"]/div[2]/div/div/div/div/div/div['
+                                                             '2]/div/div/table')))
+        exchange_table = self.driver.find_element(By.XPATH, '//*[@id="main"]/div[2]/div/div/div/div/div/div['
+                                                            '2]/div/div/table')
+
+        exchange_rates = {}
+        for row in exchange_table.find_elements(By.TAG_NAME, 'tr'):
+            if row.find_elements(By.TAG_NAME, 'td'):
+                exchange_rates[row.find_element(By.TAG_NAME, 'td').text] = row.find_elements(By.TAG_NAME, 'td')[2].text
+        return exchange_rates
+
+    def get_exchange_rate(self, currency: str) -> float:
+        r"""
+        Returns the exchange rate for the given currency.
+
+        :param str currency: The currency to get the exchange rate for (i.e. 'CHF')
+        :return float exchange_rate: The exchange rate for the given currency
+        """
+        return float(self._get_exchange_rates()[currency])
---
+++ tests/ExchangeRateCalculator_test.py
@@ -0,0 +1,27 @@
+import unittest
+from unittest.mock import MagicMock, patch
+from main import ExchangeRateCalculator
+
+
+class TestExchangeRateCalculator(unittest.TestCase):
+
+    @patch('main.webdriver.Chrome')
+    def setUp(self, mock_webdriver):
+        self.calculator = ExchangeRateCalculator()
+
+    def test_get_last_updated(self):
+        mock_element = MagicMock()
+        mock_element.text = 'Last updated on 02/03/2023'
+        self.calculator.driver.find_element.return_value = mock_element
+
+        last_updated = self.calculator.get_last_updated()
+
+        self.assertEqual(last_updated, '02/03/2023')
+
+    def test_get_exchange_rate(self):
+        mock_exchange_rates = {'CHF': '1.1012', 'USD': '1.1696'}
+        self.calculator._get_exchange_rates = MagicMock(return_value=mock_exchange_rates)
+
+        exchange_rate = self.calculator.get_exchange_rate('CHF')
+
+        self.assertEqual(exchange_rate, 1.1012)
